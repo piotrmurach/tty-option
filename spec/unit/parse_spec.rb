e45e3177fe9e412a35cffc26f12ef82f4d6896fa
Piotr Murach <pmurach@gmail.com>
2020-03-25 23:54:30 +0000

    Change command setup to be more revealing and compact

--- spec/unit/parse_spec.rb
+++ spec/unit/parse_spec.rb
@@ -7,52 +7,50 @@ def command(&block)
   Command
 end
 
+def new_command(&block)
+  command(&block).new
+end
+
 RSpec.describe TTY::Option do
   context "argument" do
     it "reads a single argument" do
-      command do
+      cmd = new_command do
         argument :foo
       end
 
-      cmd = Command.new
-      argv = %w[bar]
-      cmd.parse(argv)
+      cmd.parse(%w[bar])
 
       expect(cmd.params[:foo]).to eq("bar")
     end
 
     it "defauls argument to a value" do
-      command do
+      cmd = new_command do
         argument(:foo) { default "bar" }
       end
 
-      cmd = Command.new
       cmd.parse([])
 
       expect(cmd.params[:foo]).to eq("bar")
     end
 
     it "defauls argument to a proc" do
-      command do
+      cmd = new_command do
         argument(:foo) { default -> { "bar" } }
       end
 
-      cmd = Command.new
       cmd.parse([])
 
       expect(cmd.params[:foo]).to eq("bar")
     end
 
     it "reads an argument with exact number of values" do
-      command do
+      cmd = new_command do
         argument(:foo) { arity 3 }
 
         argument :bar
       end
 
-      cmd = Command.new
-      argv = %w[x y z w]
-      cmd.parse(argv)
+      cmd.parse(%w[x y z w])
 
       expect(cmd.params[:foo]).to eq(%w[x y z])
       expect(cmd.params[:bar]).to eq("w")
@@ -69,30 +67,26 @@ RSpec.describe TTY::Option do
 
   context "keyword" do
     it "reads a keyword with a single value" do
-      command do
+      cmd = new_command do
         keyword :foo
 
         keyword :bar
       end
 
-      cmd = Command.new
-      argv = %w[foo=x bar=12]
-      cmd.parse(argv)
+      cmd.parse(%w[foo=x bar=12])
 
       expect(cmd.params[:foo]).to eq("x")
       expect(cmd.params[:bar]).to eq("12")
     end
 
     it "collects multiple keyword occurences" do
-      command do
+      cmd = new_command do
         keyword :foo, arity: 2
 
         keyword :bar, convert: :int
       end
 
-      cmd = Command.new
-      argv = %w[foo=x bar=12 foo=y]
-      cmd.parse(argv)
+      cmd.parse(%w[foo=x bar=12 foo=y])
 
       expect(cmd.params[:foo]).to eq(%w[x y])
       expect(cmd.params[:bar]).to eq("12")
