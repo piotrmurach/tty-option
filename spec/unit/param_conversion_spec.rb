7fd52a44250492a3e4f97d1f0463099e9529a5d8
Piotr Murach <pmurach@gmail.com>
2020-04-03 23:49:31 +0100

    Check converion error propagation

--- spec/unit/param_conversion_spec.rb
+++ spec/unit/param_conversion_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe TTY::Option::ParamConversion do
     expect(described_class[param, "12"]).to eq(12)
   end
 
+  it "fails to convert parameter value to expected type" do
+    param = TTY::Option::Parameter::Argument.create(:foo, convert: :int)
+
+    expect {
+      described_class[param, "bar"]
+    }.to raise_error(TTY::Option::InvalidConversionArgument,
+                    "Invalid value of \"bar\" for :int conversion")
+  end
+
   it "converts parameter value to expected type with a block" do
     param = TTY::Option::Parameter::Argument.create(:foo,
                                                     convert: ->(val) { val.to_i })
