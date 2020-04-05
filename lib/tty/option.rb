7862bb96b4eaa67b44dec7821c49a4eb9207998a
Piotr Murach <pmurach@gmail.com>
2020-04-05 19:48:38 +0100

    Change to extract errors

--- lib/tty/option.rb
+++ lib/tty/option.rb
@@ -3,49 +3,12 @@
 require_relative "option/arity_dsl"
 require_relative "option/conversions"
 require_relative "option/dsl"
+require_relative "option/errors"
 require_relative "option/parser"
 require_relative "option/version"
 
 module TTY
   module Option
-    Error = Class.new(StandardError)
-
-    # Raised when an option matches more than one parameter option
-    AmbiguousOption = Class.new(Error)
-
-    # Raised when overriding already defined conversion
-    ConversionAlreadyDefined = Class.new(Error)
-
-    # Raised when argument doesn't match expected value
-    InvalidArgument = Class.new(Error)
-
-    # Raised when number of arguments doesn't match
-    InvalidArity = Class.new(Error)
-
-    # Raised when conversion provided with unexpected argument
-    InvalidConversionArgument = Class.new(Error)
-
-    # Raised when found unrecognized option
-    InvalidOption = Class.new(Error)
-
-    # Raised when permitted type is incorrect
-    InvalidPermitted = Class.new(Error)
-
-    # Raised when validation format is not supported
-    InvalidValidation = Class.new(Error)
-
-    # Raised when option requires an argument
-    MissingArgument = Class.new(Error)
-
-    # Raised when attempting to register already registered parameter
-    ParameterConflict = Class.new(Error)
-
-    # Raised when conversion type isn't registered
-    UnsupportedConversion = Class.new(Error)
-
-    # Raised when argument value isn't permitted
-    UnpermittedArgument = Class.new(Error)
-
     # Enhance object with command line option parsing
     #
     # @api public
---
+++ lib/tty/option/errors.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module TTY
+  module Option
+    Error = Class.new(StandardError)
+
+    # Raised when an option matches more than one parameter option
+    AmbiguousOption = Class.new(Error)
+
+    # Raised when overriding already defined conversion
+    ConversionAlreadyDefined = Class.new(Error)
+
+    # Raised when argument doesn't match expected value
+    InvalidArgument = Class.new(Error)
+
+    # Raised when number of arguments doesn't match
+    InvalidArity = Class.new(Error)
+
+    # Raised when conversion provided with unexpected argument
+    InvalidConversionArgument = Class.new(Error)
+
+    # Raised when found unrecognized option
+    InvalidOption = Class.new(Error)
+
+    # Raised when permitted type is incorrect
+    InvalidPermitted = Class.new(Error)
+
+    # Raised when validation format is not supported
+    InvalidValidation = Class.new(Error)
+
+    # Raised when option requires an argument
+    MissingArgument = Class.new(Error)
+
+    # Raised when attempting to register already registered parameter
+    ParameterConflict = Class.new(Error)
+
+    # Raised when conversion type isn't registered
+    UnsupportedConversion = Class.new(Error)
+
+    # Raised when argument value isn't permitted
+    UnpermittedArgument = Class.new(Error)
+  end # Option
+end # TTY
