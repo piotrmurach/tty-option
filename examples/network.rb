30b78f20c77be1a565201db9f0dce7572b0f65b4
Piotr Murach <pmurach@gmail.com>
2020-04-26 18:18:32 +0100

    Change network example to add help display and option descriptions

--- examples/network.rb
+++ examples/network.rb
@@ -6,38 +6,57 @@ module Network
   class Create
     include TTY::Option
 
+    header "CLI app v1.2.3"
+
+    banner "#{program} create [OPTIONS] NETWORK"
+
+    desc "Create a network"
+
+    footer "Run 'network create --help' for more information on a command."
+
     argument :network do
       required
     end
 
-    flag :attachable
+    flag :attachable do
+      desc "Enable manual container attachment"
+    end
 
-    flag :config_only
+    flag :config_only do
+      desc "Create a configuration only network"
+    end
 
     option :driver do
       short "-d"
       long "--driver string"
+      default "bridge"
+      desc "Driver to manage the Network"
     end
 
     option :gateway do
       long "--gateway strings"
       convert :list
+      desc "IPv4 or IPv6 Gateway for the master subnet"
     end
 
     option :label do
       long "--label list"
       convert :list
+      desc "Set metadata on a network"
     end
 
     option :options do
       short "-o"
       long "--opt map"
       convert :map
+      default({})
+      desc "Set driver specific options"
     end
 
     option :subnet do
       long "--subnet strings"
       convert :list
+      desc "Subnet in CIDR format that represents a network segment"
     end
 
     def execute
@@ -51,3 +70,5 @@ create = Network::Create.new
 create.parse(%w[my-network --gateway host --driver overlay --opt a:1 b:2])
 
 create.execute
+
+puts create.help
