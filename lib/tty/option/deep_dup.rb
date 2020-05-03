eddd275cf27952d6a7ef9cf7433f1f8c5e3a8854
Piotr Murach <pmurach@gmail.com>
2020-05-03 17:38:12 +0100

    Add ability to make a deep copy of any Ruby built-in object

---
+++ lib/tty/option/deep_dup.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module TTY
+  module Option
+    module DeepDup
+      NONDUPLICATABLE = [
+        Symbol, TrueClass, FalseClass, NilClass, Numeric, Method
+      ].freeze
+
+      # Duplicate an object making a deep copy
+      #
+      # @param [Object] object
+      #
+      # @api public
+      def self.deep_dup(object)
+        case object
+        when String then object.dup
+        when *NONDUPLICATABLE then object
+        when Hash   then deep_dup_hash(object)
+        when Array  then deep_dup_array(object)
+        else object.dup
+        end
+      end
+
+      # A deep copy of hash
+      #
+      # @param [Hash] object
+      #
+      # @api private
+      def self.deep_dup_hash(object)
+        object.each_with_object({}) do |(key, val), new_hash|
+          new_hash[deep_dup(key)] = deep_dup(val)
+        end
+      end
+
+      # A deep copy of array
+      #
+      # @param [Array] object
+      #
+      # @api private
+      def self.deep_dup_array(object)
+        object.each_with_object([]) do |val, new_array|
+          new_array << deep_dup(val)
+        end
+      end
+    end # DeepDup
+  end # Option
+end # TTY
---
+++ spec/unit/deep_dup_spec.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+require "bigdecimal"
+require "set"
+
+require_relative "../../lib/tty/option/deep_dup"
+
+RSpec.describe TTY::Option::DeepDup do
+  [
+    "foo",
+    ["foo", "bar"],
+    {"foo" => "bar"},
+    Set.new(["foo", "bar"]),
+  ].each do |obj|
+    it "duplicates #{obj}" do
+      dupped_obj = described_class.deep_dup(obj)
+
+      expect(dupped_obj).to eq(obj)
+      expect(dupped_obj).to_not equal(obj)
+    end
+  end
+
+  [
+    Class.new,
+    Object.new
+  ].each do |obj|
+    it "duplicates #{obj}" do
+      dupped_obj = described_class.deep_dup(obj)
+
+      expect(dupped_obj).to_not equal(obj)
+    end
+  end
+
+  [
+    :foo,
+    11,
+    1.23,
+    true,
+    false,
+    nil,
+    method(:exec),
+    BigDecimal("1.23")
+  ].each do |obj|
+    it "doesn't duplicate #{obj.class.name}" do
+      dupped_obj = described_class.deep_dup(obj)
+
+      expect(dupped_obj).to eq(obj)
+      expect(dupped_obj).to equal(obj)
+    end
+  end
+
+  it "duplicates nested hash" do
+    obj = {
+      "foo" => {
+        "bar" => {
+          "baz" => :qux
+        }
+      }
+    }
+
+    dupped_obj = described_class.deep_dup(obj)
+
+    expect(dupped_obj).to eq(obj)
+    expect(dupped_obj).to_not equal(obj)
+    expect(dupped_obj.keys).to eq(obj.keys)
+    expect(dupped_obj.keys).to_not equal(obj.keys)
+
+    expect(dupped_obj["foo"]).to eq(obj["foo"])
+    expect(dupped_obj["foo"]).to_not equal(obj["foo"])
+    expect(dupped_obj["foo"].keys).to eq(obj["foo"].keys)
+    expect(dupped_obj["foo"].keys).to_not equal(obj["foo"].keys)
+
+    expect(dupped_obj["foo"]["bar"]).to eq(obj["foo"]["bar"])
+    expect(dupped_obj["foo"]["bar"]).to_not equal(obj["foo"]["baz"])
+    expect(dupped_obj["foo"]["bar"].keys).to eq(obj["foo"]["bar"].keys)
+    expect(dupped_obj["foo"]["bar"].keys).to_not equal(obj["foo"]["bar"].keys)
+  end
+
+  it "duplicates nested array" do
+    obj = [
+      "foo",
+      11,
+      :bar,
+      ["baz", {qux: {"quux" => true}}],
+      false
+    ]
+    dupped_obj = described_class.deep_dup(obj)
+
+    expect(dupped_obj).to eq(obj)
+    expect(dupped_obj).to_not equal(obj)
+
+    # "foo" is duplicated
+    expect(dupped_obj[0]).to eq(obj[0])
+    expect(dupped_obj[0]).to_not equal(obj[0])
+
+    # 11 is not duplicated
+    expect(dupped_obj[1]).to eq(obj[1])
+    expect(dupped_obj[1]).to equal(obj[1])
+
+    # :bar is not duplicated
+    expect(dupped_obj[2]).to eq(obj[2])
+    expect(dupped_obj[2]).to equal(obj[2])
+
+    # array is duplicated
+    expect(dupped_obj[3]).to eq(obj[3])
+    expect(dupped_obj[3]).to_not equal(obj[3])
+
+    # "baz" in array is duplicated
+    expect(dupped_obj[3][0]).to eq(obj[3][0])
+    expect(dupped_obj[3][0]).to_not equal(obj[3][0])
+
+    # hash in array is duplicated
+    expect(dupped_obj[3][1]).to eq(obj[3][1])
+    expect(dupped_obj[3][1]).to_not equal(obj[3][1])
+
+    # false is not duplicated
+    expect(dupped_obj[4]).to eq(obj[4])
+    expect(dupped_obj[4]).to equal(obj[4])
+  end
+end
