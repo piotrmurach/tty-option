47abf0c51b6936688694456a199535c307cffa88
Piotr Murach <pmurach@gmail.com>
2021-08-04 14:25:45 +0200

    Add documentation to the result class

--- lib/tty/option/result.rb
+++ lib/tty/option/result.rb
@@ -18,29 +18,51 @@ module TTY
         Failure.new(value)
       end
 
+      # Wrapped value
+      #
+      # @api public
       attr_reader :value
 
+      # Reason for failure
+      #
+      # @api public
       attr_reader :error
 
+      # Check whether or not a result is a success monad
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def success?
         is_a?(Success)
       end
 
+      # Check whether or not a result is a failure class
+      #
+      # @return [Boolean]
+      #
+      # @api public
       def failure?
         is_a?(Failure)
       end
 
+      # Success monad containing a value
+      #
+      # @api private
       class Success < Result
         def initialize(value)
           @value = value
         end
       end
 
+      # Failure monad containing an error
+      #
+      # @api private
       class Failure < Result
         def initialize(error)
           @error = error
         end
       end
-    end
-  end
-end
+    end # Result
+  end # Option
+end # TTY
