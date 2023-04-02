87dffb34f64197c99cc7ab1263978490b3357788
Piotr Murach <pmurach@gmail.com>
2023-04-02 16:01:10 +0100

    Change rspec configuration to stop truncating formatted output on failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,13 +43,15 @@ end
 
 RSpec.configure do |config|
   config.include(Helpers)
+
+  config.expect_with :rspec do |expectations|
+    expectations.max_formatted_output_length = nil
+    expectations.syntax = :expect
+  end
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
 end
